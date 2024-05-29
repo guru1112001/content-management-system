1e077f1060f218da4a9d039bf21f31ee934db126
Gurmeet Singh <gbhatia266@gmail.com>
2024-05-29 17:33:17 +0530

    added the syllabus api and teaching material api

--- app/Models/TeachingMaterial.php
+++ app/Models/TeachingMaterial.php
@@ -9,14 +9,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class TeachingMaterial extends Model
 {
     use HasFactory;
-    protected $fillable=[
-        'name',
-        'file',
-        'published',
-        'section_id'
+    protected $fillable = [
+        'section_id', 'name', 'material_source', 'file', 'content', 
+        'unlimited_view', 'maximum_views', 'prerequisite', 'description', 
+        'privacy_allow_access', 'privacy_downloadable', 'published', 'sort', 
+        'doc_type', 'maximum_marks', 'passing_percentage', 'result_declaration', 
+        'maximum_attempts', 'general_instructions', 'start_submission', 
+        'stop_submission'
     ];
     public function section()
     {
-       return $this->belongsTo(Section::class);
+       return $this->belongsTo(Section::class,'section_id');
     }
 }
